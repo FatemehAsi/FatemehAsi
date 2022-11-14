ce08647aa7e03712f3dfe4f66ea17bd5e3402009
Fatemeh <fatemeh.asi.2000@gmail.com>
2022-11-15 01:34:20 +0330

    exercise4 added

---
+++ exercise4/script.js
@@ -0,0 +1,6 @@
+const firstNumber = Number(prompt ("Enter first number : "));
+const secondNumber = Number(prompt ("Enter second number : "));
+const thirdNumber = Number(prompt ("Enter third number : "));
+const average = (firstNumber + secondNumber + thirdNumber) / 3;
+console.log(`The average is ${average}`);
+
