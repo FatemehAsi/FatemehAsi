e3daa2c3fbb3bfe3c76d4cb10d0f38f62fe0a0b0
Fatemeh Asi <108950539+FatemehAsi@users.noreply.github.com>
2023-03-23 00:29:43 +0330

    Delete script.js

--- exercise3/script.js
+++
@@ -1,9 +0,0 @@
-const birthdayYear = prompt ("Enter your age : ");
-const age = 1401 - birthdayYear;
-console.log(`سن شما : ${age}`)
-if(age < 18){
-    console.log(`کودک`);
-}else{
-    console.log(`بزرگسال`);
-}
-
